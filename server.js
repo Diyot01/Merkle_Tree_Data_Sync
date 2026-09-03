fc59d14b5403994d5690eebcc0542f7c8e3275bd
Diyot01 <ayushpersonal2005@gmail.com>
2026-09-03 23:57:51 +0530

    Fix port config, add missing db connection file, add start script

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const connectDB = require('./config/db');
 
 connectDB();   
 
-server.listen(3000, () => {
-    console.log("server running at 3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`server running at ${PORT}`);
 });
